f8645a89fe58793e326a2403de26e460db7d4512
oOBlinkOo <rivensona@gmail.com>
2017-02-14 08:38:25 -0600

    first commit

---
+++ src/router.ts
@@ -0,0 +1,9 @@
+import * as LoginController from './controller/loginController';
+
+
+import express = require('express');
+
+export function defineRoutes(app: express.Application) {
+  app.use('/login', LoginController.router);
+
+}
