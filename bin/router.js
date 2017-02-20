02389605ab641cd630e4ea279a82d29f1b3a017a
oOBlinkOo <rivensona@gmail.com>
2017-02-20 17:08:06 -0600

    veamos

--- bin/router.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var UserController = require("./controller/UserController");
-function defineRoutes(app) {
-    app.use('/user', UserController.router);
-}
-exports.defineRoutes = defineRoutes;
